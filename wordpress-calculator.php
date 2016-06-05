eefbc10cda6f1396029764858f277a992b738588
unknown <prakash01@Fazil-PC.VACKER.NET>
2016-06-05 13:11:25 +0400

    Added plugin indexing file

---
+++ wordpress-calculator.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * @package WordPress Calculator
+ * @version 1.0
+ */
+/*
+Plugin Name: WordPress Calculator
+Plugin URI:
+Description: This plugins is for calculation purpose in web. This is WordPress plugin. Only, WordPress platform will be supported by this plugin.
+Author: Prakash Kumar Luitel
+Version: 1.0
+Author URI: http://lprakash.com.np/
+*/
